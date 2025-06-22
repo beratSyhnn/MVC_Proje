2c48a3db92ca1066107a5ba6eef8162ef9bc30ba
beratSyhnn <berat.sayhan@icloud.com>
2025-06-22 17:56:21 +0300

    İlk commit - Spor Üye Takip Sistemi

---
+++ SporUyeTakipSistemi/Controllers/UyeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SporUyeTakipSistemi.Models;
+
+namespace SporUyeTakipSistemi.Controllers
+{
+    public class UyeController : Controller
+    {
+       
+        static List<Uye> uyeler = new List<Uye>();
+
+       
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Kayit(Uye model)
+        {
+            uyeler.Add(model);
+            return RedirectToAction("Listele");
+        }
+
+      
+        public ActionResult Listele()
+        {
+            return View(uyeler);
+        }
+    }
+}
---
+++ SporUyeTakipSistemi/Models/Uye.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SporUyeTakipSistemi.Models
+{
+    public class Uye
+    {
+        public int Id { get; set; }                    
+        public string Ad { get; set; }                  
+        public string Soyad { get; set; }              
+        public string TCKimlik { get; set; }            
+        public DateTime KayitTarihi { get; set; }       
+        public DateTime BitisTarihi { get; set; }       
+
+       
+        public int KalanGun
+        {
+            get
+            {
+                int kalan = (BitisTarihi - DateTime.Now).Days;
+                return kalan < 0 ? 0 : kalan;
+            }
+        }
+    }
+}
